6bf965a56776a95474b852063717aab077638121
adeehilman <adehilman2002@gmail.com>
2023-11-20 14:11:02 +0700

    testing

--- app/Http/Controllers/API/Forklift/IOT/QrController.php
+++ app/Http/Controllers/API/Forklift/IOT/QrController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\API\Forklift\IOT;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
 use Illuminate\Support\Facades\DB;
 use JWTAuth;
 use Tymon\JWTAuth\Exceptions\JWTException;
