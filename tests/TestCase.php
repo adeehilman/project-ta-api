4375c69041d2918407db31fdc6944be2c7e9d4b7
adeehilman <adehilman2002@gmail.com>
2024-01-08 09:12:07 +0700

    backup api

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,62 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-
+use Akaunting\Firewall\Provider;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->setUpDatabase();
+
+        $this->setUpConfig();
+
+        $this->artisan('vendor:publish', ['--tag' => 'firewall']);
+        $this->artisan('migrate:refresh', ['--database' => 'testbench']);
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            Provider::class,
+        ];
+    }
+
+    protected function setUpDatabase()
+    {
+        config(['database.default' => 'testbench']);
+
+        config(['database.connections.testbench' => [
+                'driver'   => 'sqlite',
+                'database' => ':memory:',
+                'prefix'   => '',
+            ],
+        ]);
+    }
+
+    protected function setUpConfig()
+    {
+        config(['firewall' => require(__DIR__ . '/config/firewall.php')]);
+
+        config(['firewall.notifications.mail.enabled' => false]);
+        config(['firewall.middleware.ip.methods' => ['all']]);
+        config(['firewall.middleware.lfi.methods' => ['all']]);
+        config(['firewall.middleware.rfi.methods' => ['all']]);
+        config(['firewall.middleware.sqli.methods' => ['all']]);
+        config(['firewall.middleware.xss.methods' => ['all']]);
+    }
+
+    public function getNextClosure()
+    {
+        return function () {
+            return 'next';
+        };
+    }
 }
